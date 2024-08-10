32d46695f10d534fbee477de744bb3ec4562ebd0
shreyamali01 <shreya.mali@iitgn.ac.in>
2024-08-10 20:40:15 +0530

    fibonacci using tabulation

---
+++ problems/dp/2.fibo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int foo(int n){
+    int prev1 = 0;
+    int prev2 = 1;
+
+    for(int i=2; i<n; i++){
+        int curr = prev1 + prev2;
+        prev1 = curr;
+        prev2 = prev1;
+    }
+
+    return prev1;
+}
+
+int main(){
+    int n = 6;
+    int x = foo(n);
+    cout << x;
+}
